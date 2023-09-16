a05a06eb6e03b3c55857ca188af4ed641b795ff1
MiniCyberBrush <144251674+MiniCyberBrush@users.noreply.github.com>
2023-09-16 14:15:29 +0500

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout << " +--^----------,--------,-----,--------^-, "<<endl;
+cout << "  | |||||||||   '--------'     |          o"<<endl; 
+cout << "  '+---------------------------^----------|"<<endl;
+cout << "   ',----------,--------,---------------'  "<<endl;
+cout << "     // XXXXXXX //'|       //'                "<<endl; 
+cout << "    // XXXXXXX //  |      //'                 "<<endl; 
+cout << "   // XXXXXXX //'-------'                    "<<endl; 
+cout << "  // XXXXXXX //                              "<<endl; 
+cout << " // XXXXXXX //                              "<<endl; 
+cout << "//_________//                              "<<endl; 
+cout << "  '-------'                                 "<<endl; 
+
+ 
+
+
+
+ 
+ 
+
+
+
+
+   
+}
